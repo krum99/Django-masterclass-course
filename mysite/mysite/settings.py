1ce0c58c34f2bcf73131bb0577ca78ccbb02b13c
krum99 <79230210+krum99@users.noreply.github.com>
2025-12-20 22:50:19 +0200

    chore: set dir path for saving profile pictures

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = 'myapp:index'
 
 LOGIN_URL = 'login'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'pictures')
+
+MEDIA_URL = '/pictures/'
