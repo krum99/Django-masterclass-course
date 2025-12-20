94ec1da8d6c92f9953c871cecb3479ebfffb1409
krum99 <79230210+krum99@users.noreply.github.com>
2025-12-20 17:11:59 +0200

    feat(register): implement register functionality

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 
 # Create your views here.
 
 def register(request):
-  return HttpResponse('test info')
+  form = UserCreationForm(request.POST or None)
+  if request.method == "POST":
+    if form.is_valid():
+      username = form.cleaned_data.get('username')
+      messages.success(request, f'Welcome {username}, your account has been successfully created')
+      form.save()
+      return redirect('myapp:index')
+
+  
+  return  render(request, 'users/register.html', {'form': form})
