4295ffa164b75842086d67ebe22fbe8ecfadadb3
krum99 <79230210+krum99@users.noreply.github.com>
2025-12-20 18:40:23 +0200

    feat(register): add email field

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-
+from .forms import RegisterForm
 
 # Create your views here.
 
 def register(request):
-  form = UserCreationForm(request.POST or None)
+  form = RegisterForm(request.POST or None)
   if request.method == "POST":
     if form.is_valid():
       username = form.cleaned_data.get('username')
