b7433ca251268944a4e272aab5667f120eddb8a8
krum99 <79230210+krum99@users.noreply.github.com>
2025-12-20 21:24:21 +0200

    feat: implement profile page

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import RegisterForm
 from django.contrib.auth import logout
@@ -21,3 +22,8 @@ def register(request):
 def logout_view(request):
   logout(request)
   return render(request, 'users/logout.html')
+
+@login_required
+def profile(request):
+
+  return render(request, 'users/profile.html')
