5be15ead86a6e12a3c794ae8e5eae83418717200
krum99 <79230210+krum99@users.noreply.github.com>
2025-12-22 21:41:35 +0200

    update Item model

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.urls import reverse
-# Create your models here.
-
+from django.contrib.auth.models import User
 
 class Item(models.Model):
   def __str__(self):
@@ -10,6 +9,7 @@ class Item(models.Model):
   def get_absolute_url(self):
     return reverse('myapp:index')
 
+  user_name = models.ForeignKey(User, on_delete=models.CASCADE)
   item_name = models.CharField(max_length=200)
   item_desc = models.CharField()
   item_price = models.IntegerField()
