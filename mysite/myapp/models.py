93601872d09f6e429dbb64469e0433ef03ae0a68
krum99 <79230210+krum99@users.noreply.github.com>
2025-12-31 15:51:49 +0200

    update Item model

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -12,9 +12,10 @@ class Item(models.Model):
   user_name = models.ForeignKey(User, on_delete=models.CASCADE)
   item_name = models.CharField(max_length=200)
   item_desc = models.CharField()
-  item_price = models.IntegerField()
-  item_image = models.CharField(max_length=500, default='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHCva-afP1vczYjMkGUFt4QQ51zwKM2q3GcQ&s')
-
+  item_price = models.DecimalField(max_digits=4, decimal_places=2, )
+  item_image = models.URLField(max_length=500, default='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHCva-afP1vczYjMkGUFt4QQ51zwKM2q3GcQ&s')
+  is_available = models.BooleanField(default=True)
+  created_at = models.DateTimeField(auto_now_add=True)
 
 class Category(models.Model):
   name = models.CharField(max_length=100)
