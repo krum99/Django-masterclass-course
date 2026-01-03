5bf74dea0446a0c5bf5d86ca708d1013fd7ad772
krum99 <79230210+krum99@users.noreply.github.com>
2026-01-03 19:48:07 +0200

    update(Item model): add index on name

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -10,7 +10,7 @@ class Item(models.Model):
     return reverse('myapp:index')
 
   user_name = models.ForeignKey(User, on_delete=models.CASCADE)
-  item_name = models.CharField(max_length=200)
+  item_name = models.CharField(max_length=200, db_index=True)
   item_desc = models.CharField()
   item_price = models.DecimalField(max_digits=4, decimal_places=2, )
   item_image = models.URLField(max_length=500, default='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHCva-afP1vczYjMkGUFt4QQ51zwKM2q3GcQ&s')
