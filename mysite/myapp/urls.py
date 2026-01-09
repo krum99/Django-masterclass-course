01660fad460c2cd855e7bfa68603b2693b4b05fd
krum99 <79230210+krum99@users.noreply.github.com>
2026-01-09 23:11:18 +0200

    feat: implement url cahing pattern

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 
+from django.views.decorators.cache import cache_page
+
 app_name='myapp'
 
 urlpatterns = [
-  path('',views.index, name='index'),
+  path('',cache_page(15 * 60) (views.index), name='index'),
   path('<int:pk>/', views.FoodDetail.as_view(), name='detail'),
   path('add/', views.ItemCreateView.as_view(), name='create_item'),
   path('update/<int:pk>/', views.ItemUpdateView.as_view(), name='update'),
