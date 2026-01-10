9089f6a3ff8f8753b680da7877308d9d5f4325e1
krum99 <79230210+krum99@users.noreply.github.com>
2026-01-10 19:02:02 +0200

    create discount custom template filter

--- mysite/myapp/templatetags/custom_filters.py
+++ mysite/myapp/templatetags/custom_filters.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter
 def currency(value):
   return f"${value}"
+
+
+@register.filter
+def discount(value, percentage):
+  return int(value) - (int(value) * (int(percentage) / 100))
