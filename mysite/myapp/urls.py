c38e35a802fd87a37f05b20d92a78a7a5ce1669c
krum99 <79230210+krum99@users.noreply.github.com>
2026-01-10 19:16:35 +0200

    refactor: back to function view for creating item

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -8,7 +8,7 @@ app_name='myapp'
 urlpatterns = [
   path('', views.index, name='index'),
   path('<int:id>/', views.detail, name='detail'),
-  path('add/', views.ItemCreateView.as_view(), name='create_item'),
+  path('add/', views.create_item, name='create_item'),
   path('update/<int:pk>/', views.ItemUpdateView.as_view(), name='update'),
   path('delete/<int:pk>/', views.ItemDeleteView.as_view(), name='delete'),
 ]
