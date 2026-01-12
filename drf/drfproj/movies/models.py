770abd0c79fde963a003963bb8a0783c5bdab4cf
krum99 <79230210+krum99@users.noreply.github.com>
2026-01-12 21:54:34 +0200

    feat: create MovieData model
    Also register it so it is visible in the admin panel.
    Migrations done.

--- drf/drfproj/movies/models.py
+++ drf/drfproj/movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class MovieData(models.Model):
+
+  def __str__(self):
+    return self.name
+
+  name = models.CharField(max_length=250)
+  duration = models.FloatField()
+  rating = models.FloatField()
