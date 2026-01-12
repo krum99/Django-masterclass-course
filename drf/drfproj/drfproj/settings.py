82486d109c9298faa70b1f3f79aa2e31c7b8c04e
krum99 <79230210+krum99@users.noreply.github.com>
2026-01-12 21:53:47 +0200

    add movies app to installed apps

--- drf/drfproj/drfproj/settings.py
+++ drf/drfproj/drfproj/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
+    'movies.apps.MoviesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
