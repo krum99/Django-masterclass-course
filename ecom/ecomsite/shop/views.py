dcab4d240ddf25af457be7392f9413c6ed1f89c0
krum99 <79230210+krum99@users.noreply.github.com>
2026-02-23 23:33:45 +0200

    feat: impl pagination

--- ecom/ecomsite/shop/views.py
+++ ecom/ecomsite/shop/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from .models import Product
 
+from django.core.paginator import Paginator
+
 def index(request: HttpRequest) -> HttpResponse:
     products = Product.objects.all()
 
+    #search functionality
     item_name = request.GET.get('item_name')
     if item_name != '' and item_name is not None:
         products = products.filter(title__icontains=item_name)
     
-    return render(request, 'shop/index.html', {'products': products})
+    #pagination functionality
+    paginator = Paginator(products, 4)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'shop/index.html', {'products': page_obj})
