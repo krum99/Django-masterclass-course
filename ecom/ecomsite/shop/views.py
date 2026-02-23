b1d2cc9cdc52d07f844ae96dddb5eef33d2b6dd0
krum99 <79230210+krum99@users.noreply.github.com>
2026-02-23 23:04:45 +0200

    feat: search functionallity

--- ecom/ecomsite/shop/views.py
+++ ecom/ecomsite/shop/views.py
@@ -3,4 +3,9 @@ from .models import Product
 
 def index(request: HttpRequest) -> HttpResponse:
     products = Product.objects.all()
+
+    item_name = request.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        products = products.filter(title__icontains=item_name)
+    
     return render(request, 'shop/index.html', {'products': products})
