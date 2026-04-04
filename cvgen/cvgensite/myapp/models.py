c1f62d2202adeef9e6b349809a28abb391b5fb11
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-04 23:38:01 +0300

    create profile model

--- cvgen/cvgensite/myapp/models.py
+++ cvgen/cvgensite/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+  name = models.CharField(max_length=60)
+  email = models.CharField(max_length=100)
+  phone = models.CharField(max_length=20)
+  degree = models.CharField(max_length=100)
+  school = models.CharField(max_length=50)
+  university = models.CharField(max_length=100)
+  summary = models.TextField(max_length=1000)
+  previous_work = models.TextField(max_length=500)
+  skills = models.CharField(max_length=200)
