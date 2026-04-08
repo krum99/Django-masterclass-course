cf29c375941b8e3698e441cabe1da1d5c04d239b
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-08 12:54:07 +0300

    feat: adding pagination to API

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -172,6 +172,8 @@ LOGGING = {
 }
 
 REST_FRAMEWORK = {
+  "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+  "PAGE_SIZE": 5,
   "DEFAULT_AUTHENTICATION_CLASSES": [
     "rest_framework_simplejwt.authentication.JWTAuthentication",
   ],
