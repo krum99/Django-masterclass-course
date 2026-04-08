f32582c62aa178813b1e4ea0a6df7a01a7182704
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-08 21:03:46 +0300

    feat: saving links to db

--- projects/04-sitescraper/sitescraper/scraper/models.py
+++ projects/04-sitescraper/sitescraper/scraper/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Link(models.Model):
+  address = models.CharField(max_length=1000, blank=True, null=True)
+  name = models.CharField(max_length=1000, blank=True, null=True)
+
+  def __str__(self):
+    return str(self.name)
--- projects/04-sitescraper/sitescraper/scraper/views.py
+++ projects/04-sitescraper/sitescraper/scraper/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
 
+from .models import Link
+
 # Create your views here.
 
 def scrape(request):
   page = requests.get('https://www.google.com')
   soup = BeautifulSoup(page.text, 'html.parser')
 
-  link_address = []
   for link in soup.find_all('a'):
-    link_address.append(link.get('href'))
+    link_address = link.get('href')
+    link_text = link.string
+    Link.objects.create(address=link_address, name=link_text)
 
-  return render(request, 'scraper/result.html', {'link_address': link_address})
+  data = Link.objects.all()
+
+  return render(request, 'scraper/result.html', {'data': data})
