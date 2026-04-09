8d2f272de8d7279efa2ba71d37bdae65b9cecb13
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-09 14:25:58 +0300

    create product model

--- projects/02-ecom/ecom2/myapp/models.py
+++ projects/02-ecom/ecom2/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+  name = models.CharField(max_length=100)
+  price = models.FloatField()
+  # discounted_price = 
+  description = models.TextField()
+  image = models.ImageField(upload_to='images/')
+  slug = models.SlugField(max_length=100)
+  stock = models.IntegerField()
+  active = models.BooleanField()
