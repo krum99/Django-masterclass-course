0fe0ef4837d4f665b17d623f3b3d7861bdbc790a
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-12 18:30:24 +0300

    feat(register): add redirect when user is registered

--- projects/02-ecom/ecom2/users/views.py
+++ projects/02-ecom/ecom2/users/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .forms import CreateUserForm
 
 def register(request):
   form = CreateUserForm()
+
+  if request.method == "POST":
+    form = CreateUserForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('index')
   return render(request, 'users/register.html', {'form': form})
