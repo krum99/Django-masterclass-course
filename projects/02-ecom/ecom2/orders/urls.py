87db58d2ac4c7b80b8536dda8b5e726b710e7295
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-14 00:42:29 +0300

    feat: address endpoints

--- projects/02-ecom/ecom2/orders/urls.py
+++ projects/02-ecom/ecom2/orders/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .import views
 
 urlpatterns = [
-  
+  path('add-address', views.add_address, name='add_address'),
 ]
--- projects/02-ecom/ecom2/orders/views.py
+++ projects/02-ecom/ecom2/orders/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import AddressForm
+from .models import Address
 # Create your views here.
+
+def add_address(request):
+    try:
+        address = Address.objects.get(user=request.user)
+    except Address.DoesNotExist:
+        address=None
+    if request.method=="POST":
+        form = AddressForm(request.POST)
+        if form.is_valid():
+            address = form.save(commit=False)
+            address.user = request.user
+            address.save()
+            return redirect("index")
+    form = AddressForm(instance=address)
+    return render(request,'orders/add_address.html',{'form':form})
