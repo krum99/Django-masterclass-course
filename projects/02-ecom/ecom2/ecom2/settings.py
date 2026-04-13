291c0be9aaddab540b63f68634cd653b2d27f0f7
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-13 16:41:27 +0300

    feat: setup auto sending email verification

--- projects/02-ecom/ecom2/ecom2/settings.py
+++ projects/02-ecom/ecom2/ecom2/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = 'static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+
+#EMAIL_BACKEND ='django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS ='True'
+EMAIL_HOST_USER ='stoyanovkrum99@gmail.com'
+EMAIL_HOST_PASSWORD="mmzkjghstrmfywzl"
--- projects/02-ecom/ecom2/users/views.py
+++ projects/02-ecom/ecom2/users/views.py
@@ -1,14 +1,53 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
 from .forms import CreateUserForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes,force_str
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode,urlsafe_base64_encode
+from django.contrib.auth.models import User
+from .token import account_activation_token
 
 def register(request):
-  form = CreateUserForm()
+    form = CreateUserForm()
+    if request.method=="POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            #Email verification logic
+            subject = 'Verify your email to activate account'
+            message = render_to_string('users/email-verification.html',{
+                'user':user,
+                'domain':current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':account_activation_token.make_token(user),
+            })
+            user.email_user(subject=subject,message=message)
+            return redirect('email-verification-sent')
 
-  if request.method == "POST":
-    form = CreateUserForm(request.POST)
-    if form.is_valid():
-      form.save()
-      return redirect('index')
-  return render(request, 'users/register.html', {'form': form})
+            
+    return render(request,'users/register.html',{'form':form})
+
+
+def email_verification(request,uidb64,token):
+    unique_id = force_str(urlsafe_base64_decode(uidb64))
+    user = User.objects.get(pk=unique_id)
+    if user and account_activation_token.check_token(user,token):
+        user.is_active=True
+        user.save()
+        return redirect('email-verification-success')
+    else:
+        return redirect('email-verification-failed')
+
+
+def email_verification_sent(request):
+    return render(request,'users/email-verification-sent.html')
+
+def email_verification_success(request):
+    return render(request,'users/email-verification-success.html')
+
+def email_verification_failed(request):
+    return render(request,'users/email-verification-failed.html')
