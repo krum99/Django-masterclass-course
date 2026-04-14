6b1238e3144ada287887daa896518615a3a47b12
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-14 11:26:15 +0300

    create Order and OrderItem models

--- projects/02-ecom/ecom2/orders/models.py
+++ projects/02-ecom/ecom2/orders/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+from myapp.models import Product
+
 
 class Address(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -13,3 +14,19 @@ class Address(models.Model):
   state = models.CharField(max_length=100)
   postal_code = models.CharField(max_length=20)
   country = models.CharField(max_length=100)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    total_amount = models.DecimalField(max_digits=10,decimal_places=2,default=0)
+    is_paid = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.CASCADE,related_name="items")
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    @property
+    def total_price(self):
+        return self.product.price * self.quantity
