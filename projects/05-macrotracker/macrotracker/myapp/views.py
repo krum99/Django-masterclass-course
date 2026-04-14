4fb4893c13c04f73f3795ba8773992974bc5e912
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-14 21:35:14 +0300

    feat: add structure and logic for index

--- projects/05-macrotracker/macrotracker/myapp/views.py
+++ projects/05-macrotracker/macrotracker/myapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from .models import Food, Consume
 # Create your views here.
+
+def index(request):
+    if request.method=="POST":
+        #GEtting the id of item selected from our front-end
+        consumed_food_id = request.POST['food']
+        #Passing the id and getting the actual food item from our model
+        food = Food.objects.get(id=consumed_food_id)
+        # Getting the logged in user from request objecy
+        user = request.user
+        # Creating the consume object, consuming food
+        consume = Consume(user=user,food_consumed=food)
+        consume.save()
+
+    foods = Food.objects.all()
+    consumed_food = Consume.objects.filter(user=request.user)
+
+    return render(request,'myapp/index.html',{'foods':foods,'consumed_food':consumed_food})
