da4626661e767e667c357c51043749fa2a1edcb2
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-17 18:24:01 +0300

    feat: impl post request for login

--- projects/06-socialapp/socialapp/users/views.py
+++ projects/06-socialapp/socialapp/users/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from .forms import LoginForm
-# Create your views here.
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+
 
 def user_login(request):
-  form = LoginForm()
-  return render(request, 'users/login.html', {'form': form})
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                request, username=data['username'], password=data['password'])
+            if user is not None:
+                login(request, user)
+                return HttpResponse("user authenticated and logged in")
+            else:
+                return HttpResponse('Invalid credentials')
+
+    else:
+        form = LoginForm()
+    return render(request, 'users/login.html', {'form': form})
