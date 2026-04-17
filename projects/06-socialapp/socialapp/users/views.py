3fec4f307e7b49ac81c04196b4c2cb84e53a1c8d
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-17 19:33:14 +0300

    add login required

--- projects/06-socialapp/socialapp/users/views.py
+++ projects/06-socialapp/socialapp/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import LoginForm
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
 def user_login(request):
@@ -21,5 +22,6 @@ def user_login(request):
         form = LoginForm()
     return render(request, 'users/login.html', {'form': form})
 
+@login_required
 def index(request):
     return render(request, 'users/index.html')
