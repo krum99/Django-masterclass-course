eda93d3d810860260d58177d3e59e6a567365f6a
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-21 21:19:08 +0300

    feat: implement like functionality

--- projects/06-socialapp/socialapp/posts/views.py
+++ projects/06-socialapp/socialapp/posts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect   
 from .forms import PostCreateForm
 from django.contrib.auth.decorators import login_required
 from .models import Post
+from django.shortcuts import get_object_or_404
 
 @login_required
 def post_create(request):
@@ -17,5 +18,16 @@ def post_create(request):
 
 
 def feed(request):
-    posts =  Post.objects.all()
-    return render(request, 'posts/feed.html', {'posts': posts})
+    posts = Post.objects.all()
+    logged_user = request.user
+    return render(request, 'posts/feed.html', {'posts': posts, 'logged_user': logged_user})
+
+
+def like_post(request):
+    post_id = request.POST.get('post_id')
+    post = get_object_or_404(Post, id=post_id)
+    if post.liked_by.filter(id=request.user.id).exists():
+        post.liked_by.remove(request.user)
+    else:
+        post.liked_by.add(request.user)
+    return redirect('feed')
