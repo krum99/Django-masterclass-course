3a88f5d09217a915b420485d034e348086b914d9
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-22 21:36:47 +0300

    feat: calculate and vizualise yearly monthly and weekly expenses

--- projects/07-expensetracker/expensetracker/myapp/views.py
+++ projects/07-expensetracker/expensetracker/myapp/views.py
@@ -2,45 +2,63 @@ from django.shortcuts import render, redirect
 from .forms import ExpenseForm
 from .models import Expense
 from django.db.models import Sum
+import datetime
+
 
 def get_expense_summary():
-   total_expenses = Expense.objects.aggregate(Sum("amount"))['amount__sum']
+    total_expenses = Expense.objects.aggregate(Sum("amount"))["amount__sum"]
 
-   return {
-      'total_expenses': total_expenses,
-   }
+    # Logic to calculate 365 days expenses
+    last_year = datetime.date.today() - datetime.timedelta(days=365)
+    data = Expense.objects.filter(date__gt=last_year)
+    yearly_sum = data.aggregate(Sum("amount"))["amount__sum"]
 
-def index(request):
-  if request.method == "POST":
-    expense_form = ExpenseForm(request.POST)
-    if expense_form.is_valid():
-      expense_form.save()
-  else:
-    expense_form = ExpenseForm()
-    
-  expenses = Expense.objects.all()
-  summary = get_expense_summary()
+    # Monthly sum
+    last_month = datetime.date.today() - datetime.timedelta(days=30)
+    data = Expense.objects.filter(date__gt=last_month)
+    monthly_sum = data.aggregate(Sum("amount"))["amount__sum"]
+    # Weekly sum
+    last_week = datetime.date.today() - datetime.timedelta(days=7)
+    data = Expense.objects.filter(date__gt=last_week)
+    weekly_sum = data.aggregate(Sum("amount"))["amount__sum"]
 
-  context = {
-    'expense_form': expense_form,
-    'expenses': expenses,
-    **summary
+    return {
+        "total_expenses": total_expenses,
+        "yearly_sum": yearly_sum,
+        "monthly_sum": monthly_sum,
+        "weekly_sum": weekly_sum,
     }
 
-  return render(request, 'myapp/index.html', context)
+
+def index(request):
+    if request.method == "POST":
+        expense_form = ExpenseForm(request.POST)
+        if expense_form.is_valid():
+            expense_form.save()
+    else:
+        expense_form = ExpenseForm()
+
+    expenses = Expense.objects.all()
+    summary = get_expense_summary()
+
+    context = {"expense_form": expense_form, "expenses": expenses, **summary}
+
+    return render(request, "myapp/index.html", context)
+
 
 def edit(request, id):
-  expense = Expense.objects.get(id=id)
-  expense_form = ExpenseForm(instance=expense)
-  if request.method == "POST":
-    form = ExpenseForm(request.POST,instance=expense)
-    if form.is_valid():
-      form.save()
-      return redirect('index')
-  return render(request, 'myapp/edit.html', {'expense_form': expense_form})
+    expense = Expense.objects.get(id=id)
+    expense_form = ExpenseForm(instance=expense)
+    if request.method == "POST":
+        form = ExpenseForm(request.POST, instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+    return render(request, "myapp/edit.html", {"expense_form": expense_form})
+
 
 def delete(request, id):
-    if request.method=="POST":
+    if request.method == "POST":
         expense = Expense.objects.get(id=id)
         expense.delete()
-    return redirect('index')
+    return redirect("index")
