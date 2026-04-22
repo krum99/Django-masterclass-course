2bd811f7173dea50cb0205a332374ab9d84e3ca1
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-22 18:04:43 +0300

    feat: delete functionality

--- projects/07-expensetracker/expensetracker/myapp/urls.py
+++ projects/07-expensetracker/expensetracker/myapp/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns = [
   path('', views.index, name='index'),
   path('edit/<int:id>', views.edit, name='edit'),
+  path('delete/<int:id>', views.delete, name='delete'),
 ]
--- projects/07-expensetracker/expensetracker/myapp/views.py
+++ projects/07-expensetracker/expensetracker/myapp/views.py
@@ -25,3 +25,9 @@ def edit(request, id):
       form.save()
       return redirect('index')
   return render(request, 'myapp/edit.html', {'expense_form': expense_form})
+
+def delete(request, id):
+    if request.method=="POST":
+        expense = Expense.objects.get(id=id)
+        expense.delete()
+    return redirect('index')
