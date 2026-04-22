518ce15b7cf0d1f01005a0f185f4ab6fd7b1b73e
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-22 16:16:23 +0300

    feat: create form for adding expenses

--- projects/07-expensetracker/expensetracker/myapp/views.py
+++ projects/07-expensetracker/expensetracker/myapp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .forms import ExpenseForm
 
 # Create your views here.
 
 
 def index(request):
-  return render(request, 'myapp/index.html')
+  if request.method == "POST":
+    expense_form = ExpenseForm(request.POST)
+    if expense_form.is_valid():
+      expense_form.save()
+  else:
+    expense_form = ExpenseForm()
+  
+  return render(request, 'myapp/index.html', {'expense_form': expense_form})
