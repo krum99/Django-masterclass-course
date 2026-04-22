477b3c89fcae9d95436edc8e3d99ff016e279d2c
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-22 21:19:16 +0300

    fix: spell django correct

--- projects/07-expensetracker/expensetracker/expensetracker/settings.py
+++ projects/07-expensetracker/expensetracker/expensetracker/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'myapp',
-    'jango.contrib.humanize',
+    'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
