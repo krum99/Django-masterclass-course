7e8298f803f211f2347e75c5d85d56ad8eb45f4b
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-23 11:02:44 +0300

    displaying products

--- projects/09-digitalmarket/digitalmarket/myapp/views.py
+++ projects/09-digitalmarket/digitalmarket/myapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Product
 
 
 def index(request):
-    return render(request, "myapp/index.html")
+    products = Product.objects.all()
+    return render(request, "myapp/index.html", {"products": products})
