0b7686d4dbbc27a90d9c6098d5706dc07bf35035
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-25 23:21:37 +0300

    stripe integration (part 1)

--- projects/09-digitalmarket/digitalmarket/myapp/views.py
+++ projects/09-digitalmarket/digitalmarket/myapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, OrderDetail
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse
+from django.http import JsonResponse
+
+import stripe, json
 
 
 def index(request):
@@ -9,4 +15,45 @@ def index(request):
 
 def detail(request, id):
     product = Product.objects.get(id=id)
-    return render(request, "myapp/detail.html", {"product": product})
+    stripe_publishable_key = settings.STRIPE_PUBLISHABLE_KEY
+    return render(
+        request,
+        "myapp/detail.html",
+        {"product": product, "stripe_publishable_key": stripe_publishable_key},
+    )
+
+
+@csrf_exempt
+def create_checkout_session(request, id):
+    request_data = json.load(request.body)
+    product = Product.objects.get(id=id)
+    stripe_api_key = settings.STRIPE_SECRET_KEY
+    checkout_session = stripe.checkout.Session.craete(
+        customer_email=request_data["email"],
+        payment_method_types=["card"],
+        line_items=[
+            {
+                "price_data": {
+                    "currency": "usd",
+                    "product_data": {
+                        "name": product.name,
+                    },
+                    "unit_amount": int(product.price * 100),
+                },
+                "quantity": 1,
+            }
+        ],
+        mode="payment",
+        success_url=request.build_abosolute_url(reverse("success"))
+        + "?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url=request.build_abosolute_url(reverse("failed")),
+    )
+
+    order = OrderDetail()
+    order.customer_email = request_data["email"]
+    order.product = product
+    order.stripe_payment_intent = checkout_session["payment_intent"]
+    order.amount = int(product.price)
+    order.save()
+
+    return JsonResponse({"sessionId": checkout_session.id})
