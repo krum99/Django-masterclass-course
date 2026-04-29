3b7e162c2a76b38dfc4c08add928c41739434eb4
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-29 14:43:25 +0300

    feat: add daily sales for past 30 days

--- projects/09-digitalmarket/digitalmarket/myapp/views.py
+++ projects/09-digitalmarket/digitalmarket/myapp/views.py
@@ -182,6 +182,15 @@ def sales(request):
     )
     weekly_sales = data.aggregate(Sum("amount"))
 
+    # everyday sum for past 30 days
+    daily_sales_sums = (
+        OrderDetail.objects.filter(product__seller=request.user)
+        .values("created_on__date")
+        .order_by("created_on__date")
+        .annotate(sum=Sum("amount"))
+    )
+    print(daily_sales_sums)
+
     return render(
         request,
         "myapp/sales.html",
@@ -190,5 +199,6 @@ def sales(request):
             "yearly_sales": yearly_sales,
             "monthly_sales": monthly_sales,
             "weekly_sales": weekly_sales,
+            "daily_sales_sums": daily_sales_sums,
         },
     )
