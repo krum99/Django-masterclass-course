a0b96008842f065ea054ca9c26d6fb14d7518c7a
krum99 <79230210+krum99@users.noreply.github.com>
2026-04-29 12:39:28 +0300

    feat: add logout functionality

--- projects/09-digitalmarket/digitalmarket/myapp/urls.py
+++ projects/09-digitalmarket/digitalmarket/myapp/urls.py
@@ -23,4 +23,9 @@ urlpatterns = [
         auth_views.LoginView.as_view(template_name="myapp/login.html"),
         name="login",
     ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(template_name="myapp/logout.html"),
+        name="logout",
+    ),
 ]
